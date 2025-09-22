0c404bb478fe3571355e551cb04fb4b6d7e16511
IsaisSadaune <isais.sadaune@free.fr>
2025-09-22 11:43:42 +0200

    feedback damage zone

--- Assets/Scripts/DamageZoneBehaviour.cs
+++ Assets/Scripts/DamageZoneBehaviour.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using System.Collections;
-
+using DG.Tweening;
 public class DamageZoneBehaviour : MonoBehaviour
 {
     public float TimeActive;
@@ -8,11 +8,23 @@ public class DamageZoneBehaviour : MonoBehaviour
     private void Start()
     {
         if(TimeActive <= 0) TimeActive = 1f;
-        StartCoroutine(TimeAlive());
+        F_SpawnObject();
     }
+
     private IEnumerator TimeAlive()
     {
         yield return new WaitForSeconds(TimeActive);
+        F_DestroyObject();
+    }
+
+    private void F_DestroyObject()
+    {
         Destroy(gameObject);
     }
+    private void F_SpawnObject()
+    {
+        transform.DOPunchScale(Vector3.one * 1.1f, 1f, 2, 0.25f);
+        StartCoroutine(TimeAlive());
+    }
+
 }
--- Assets/Scripts/Scripts_Boss/TMP_BossPatterns.cs
+++ Assets/Scripts/Scripts_Boss/TMP_BossPatterns.cs
@@ -1,21 +1,54 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections;
+
 public class TMP_BossPatterns : MonoBehaviour
 {
     //on va dire qu'elles ont la bonne taille de base
     [SerializeField] private DamageZoneBehaviour prefabDamageZones;
-    [SerializeField] private Transform player;
-    [SerializeField] private GameObject whisp;
+    [SerializeField] private Transform playerPosition;
+    [SerializeField] private GameObject prefabWhisp;
     [SerializeField] private Rigidbody rb;
+    private Tween WhispTween;
+
 
 
     [ContextMenu("patternWhisp")]
     public void WhispPattern()
     {
-        //partie 0 : choix direction
+        GameObject _whisp = Instantiate(prefabWhisp, transform.position, Quaternion.identity);
+    }
 
-        //partie 1 : le fouet
 
-        //partie 2 : les explosions
+    [ContextMenu("damageZones")]
+    public void DamagesZones()
+    {
+        RaycastHit hit;
+        for(int i=1;i<5;i++)
+        {
+            if (Physics.Raycast(transform.position + Vector3.right * 4 * i, Vector3.down, out hit))
+            {
+                StartCoroutine(DelaySpawn(0.5f * i, hit.point));
+            }
+            if (Physics.Raycast(transform.position + Vector3.left * 5 * i, Vector3.down, out hit))
+            {
+                StartCoroutine(DelaySpawn(0.5f * i, hit.point));
+            }
+            if (Physics.Raycast(transform.position + Vector3.forward * 5 * i, Vector3.down, out hit))
+            {
+                StartCoroutine(DelaySpawn(0.5f * i, hit.point));
+            }
+            if (Physics.Raycast(transform.position + Vector3.back * 5 * i, Vector3.down, out hit))
+            {
+                StartCoroutine(DelaySpawn(0.5f * i, hit.point));
+            }
+        }
+    }
+
+
+    private IEnumerator DelaySpawn(float f, Vector3 spawnPos)
+    {
+        yield return new WaitForSeconds(f);
+        Instantiate(prefabDamageZones, spawnPos, Quaternion.identity);
     }
 }
