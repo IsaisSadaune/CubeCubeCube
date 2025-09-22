6c91d3bde3150358f9bc8fe5e5d48b4772e34aff
Rob11005 <langerobin05@gmail.com>
2025-09-22 11:01:03 +0200

    test physique

--- Assets/Scripts/StateMachine/Ability_Scripts/Dash.cs
+++ Assets/Scripts/StateMachine/Ability_Scripts/Dash.cs
@@ -1,26 +1,65 @@
 using System.Collections;
+using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
     public Player player;
+    public CapsuleCollider capsule;
+    public LayerMask obstacleMask;
+    bool obstacleInTheWay;
+    bool besideObstacle;
+    Vector3 p1;
+    Vector3 p2;
+    RaycastHit hit;
+
     public void StartDash()
     {
         StartCoroutine(DashCoroutine());
     }
-    
+
+    void FixedUpdate()
+    {
+        p1 = player.rb.position + capsule.center + Vector3.up * (capsule.height / 2f - capsule.radius);
+        p2 = player.rb.position + capsule.center - Vector3.up * (capsule.height / 2f - capsule.radius);
+
+        if (Physics.CheckCapsule(p1, p2, capsule.radius, obstacleMask))
+        {
+            besideObstacle = true;
+        }
+        else
+        {
+            besideObstacle = false;
+        }
+        if (Physics.CapsuleCast(p1, p2, capsule.radius, player.rb.transform.forward, out hit, 5f, obstacleMask))
+        {
+            obstacleInTheWay = true;
+        }
+        else
+        {
+            obstacleInTheWay = false;
+        }
+    }
+
     #region Coroutines
     public IEnumerator DashCoroutine()
     {
         player.canDash = false;
-        RaycastHit hit;
         float startTime = Time.time;
         Vector3 startPos = player.rb.position;
         Vector3 endPos;
 
-        if (Physics.Raycast(transform.position, player.rb.transform.forward, out hit, 5f))
+        p1 = player.rb.position + capsule.center + Vector3.up * (capsule.height / 2f - capsule.radius);
+        p2 = player.rb.position + capsule.center - Vector3.up * (capsule.height / 2f - capsule.radius);
+
+        if (besideObstacle)
         {
-            endPos = hit.point * 0.9f;
+            endPos = player.rb.position;
+        }
+        else if (obstacleInTheWay)
+        {
+            endPos = player.rb.position + player.rb.transform.forward * hit.distance;
         }
         else
         {
