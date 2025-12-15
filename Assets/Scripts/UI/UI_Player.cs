efd20d4b4494c484183d20a560156e41c27b8f42
Isais <52676440+IsaisSadaune@users.noreply.github.com>
2025-12-15 09:58:40 +0100

    Fix bug Thwomp + timer

--- Assets/Scripts/UI/UI_Player.cs
+++ Assets/Scripts/UI/UI_Player.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UI_Player : MonoBehaviour
 {
+    [SerializeField] private Boss_Variables bv;
     [SerializeField] private GameObject hpPrefab;
+    [SerializeField] private TextMeshProUGUI timer;
     private List<GameObject> fullHPs = new();
     private int pointeur;
+
+    private float time;
     public void SetHps(int hpsMax)
     {
         for (int i = 0; i < hpsMax; i++)
@@ -19,7 +24,7 @@ public class UI_Player : MonoBehaviour
     [ContextMenu("RemoveHP")]
     public void RemoveHP(int x)
     {
-        if (pointeur > 0)
+        if (pointeur >= 0)
             while (x > 0)
             {
                 fullHPs[pointeur].SetActive(false);
@@ -36,6 +41,19 @@ public class UI_Player : MonoBehaviour
         {
             pointeur++;
             fullHPs[pointeur].SetActive(true);
+        }
+    }
+    private void Start()
+    {
+        time = 0;
+        timer.text = time.ToString();
+    }
+    private void Update()
+    {
+        if ((UnityEngine.Object)bv != null && bv.HP > 0)
+        {
+            time += Time.deltaTime;
+            timer.text = time.ToString();
         }
     }
 
