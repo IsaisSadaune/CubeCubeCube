08373331ffff0db734710deb2ebad225a2101c3a
Preonzz <tomempis05@gmail.com>
2026-02-16 15:52:55 +0100

    Update Script for outside access

--- Assets/Scripts/Scripts_UI/UIRageBar.cs
+++ Assets/Scripts/Scripts_UI/UIRageBar.cs
@@ -1,5 +1,5 @@
-using UnityEngine;
 using MoreMountains.Tools;
+using UnityEngine;
 
 public class UIRageBar : MonoBehaviour
 {
@@ -8,8 +8,27 @@ public class UIRageBar : MonoBehaviour
     [Range(0f, 100f)] public float value;
     [MMInspectorButton("ChangeBarValue")] public bool ChangeBarValueBtn;
 
+    private void Start()
+    {
+        value = 0f;
+        ChangeBarValue();
+    }
+
     void ChangeBarValue()
     {
         rageBar.UpdateBar(value, 0f, 100f);
     }
+
+    public void IncreaseRageBar(int valueToAdd)
+    {
+        value += valueToAdd;
+        ChangeBarValue();
+    }
+
+    public void DecreaseBarValue(int valueToPull)
+    {
+        value -= valueToPull;
+        ChangeBarValue();
+    }
+
 }
