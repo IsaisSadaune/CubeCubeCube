7d5dd691ff510dce3b3e8d5f7bee2098c2bd68b3
Preonzz <tomempis05@gmail.com>
2026-02-17 00:46:32 +0100

    Fin premier jet UI Combat
    Manque link avec scripts player

--- Assets/Scripts/Scripts_UI/UIRageBar.cs
+++ Assets/Scripts/Scripts_UI/UIRageBar.cs
@@ -1,4 +1,6 @@
+using MoreMountains.Feedbacks;
 using MoreMountains.Tools;
+using System.Collections;
 using UnityEngine;
 
 public class UIRageBar : MonoBehaviour
@@ -8,10 +10,13 @@ public class UIRageBar : MonoBehaviour
     [Range(0f, 100f)] public float value;
     [MMInspectorButton("ChangeBarValue")] public bool ChangeBarValueBtn;
 
+    [SerializeField] private MMF_Player barFlickerFbPlayer;
+
     private void Start()
     {
-        value = 0f;
+        barFlickerFbPlayer = GetComponent<MMF_Player>();
         ChangeBarValue();
+        StartCoroutine(BarFlickerTrigger());
     }
 
     void ChangeBarValue()
@@ -21,14 +26,31 @@ public class UIRageBar : MonoBehaviour
 
     public void IncreaseRageBar(int valueToAdd)
     {
-        value += valueToAdd;
+        if ((value += valueToAdd) > 100)
+            value = 100;
+        else
+            value += valueToAdd;
+
         ChangeBarValue();
     }
 
     public void DecreaseBarValue(int valueToPull)
     {
-        value -= valueToPull;
+        if ((value += valueToPull) < 0)
+            value = 0;
+        else
+            value -= valueToPull;
+
         ChangeBarValue();
     }
 
+    IEnumerator BarFlickerTrigger()
+    {
+        yield return new WaitUntil(() => value == 100f);
+        barFlickerFbPlayer.PlayFeedbacks();
+
+        // Mettre le double de la durée du Holding pause entre deux swaps d'image en temps
+        yield return new WaitForSecondsRealtime(0.50f);
+        StartCoroutine(BarFlickerTrigger());
+    }
 }
