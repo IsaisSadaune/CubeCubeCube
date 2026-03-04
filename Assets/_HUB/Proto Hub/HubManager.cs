d2d15ac3d1983fe3089d362d3d9399197fa1e8ef
Rob11005 <langerobin05@gmail.com>
2026-03-04 12:17:51 +0100

    Squelette de modif du HUB

--- Assets/_HUB/Proto Hub/HubManager.cs
+++ Assets/_HUB/Proto Hub/HubManager.cs
@@ -6,10 +6,30 @@ using UnityEngine.UI;
 public class HubManager : MonoBehaviour
 {
     public bool asProc = false;
+    public GameObject[] hubPrefabs;
+    public Transform hubPos;
+    public int hubPhase = 0;
 
     [ContextMenu("transition test")]
     public void TransitionIn()
     {
                 SceneManager.LoadScene("Tutorial Scene");
+    }
+
+    //Lancer la fonction au moment de Load la scène HUB 
+    public void UpdateHub()
+    {
+        switch(hubPhase)
+        {
+            case 0 :
+                Instantiate(hubPrefabs[hubPhase], hubPos.position, Quaternion.identity);
+            break;
+            case 1 :
+                Instantiate(hubPrefabs[hubPhase], hubPos.position, Quaternion.identity);
+            break;
+            case 2 :
+                Instantiate(hubPrefabs[hubPhase], hubPos.position, Quaternion.identity);
+            break;
+        }
     }
 }
