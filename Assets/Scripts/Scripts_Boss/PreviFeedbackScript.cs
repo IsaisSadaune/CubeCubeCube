795d2f76b464a4fa0c01708616dd630e46356d6d
Preonzz <tomempis05@gmail.com>
2026-03-10 14:50:29 +0100

    Push prévi attaques

--- Assets/Scripts/Scripts_Boss/PreviFeedbackScript.cs
+++ Assets/Scripts/Scripts_Boss/PreviFeedbackScript.cs
@@ -7,7 +7,7 @@ public class PreviFeedbackScript : MonoBehaviour
     [SerializeField] private Color startColor, endColor;
     private Renderer renderer;
     private Vector3 baseScale;
-    [SerializeField] private float changeScaleDuration, changeColorDuration, timeBeforeChangingColor;
+    [SerializeField] private float changeScaleDuration, changeColorDuration, timeBeforeChangingColor, timeBeforeDestroyWhenFinished;
 
     private void Start()
     {
@@ -15,28 +15,14 @@ public class PreviFeedbackScript : MonoBehaviour
         transform.localScale = new Vector3(0.1f, transform.localScale.y, 0.1f);
         renderer = GetComponent<Renderer>();
         renderer.material.color = startColor;
-    }
 
-    private void ChangeColor()
-    {
-        renderer.material.DOColor(endColor, changeColorDuration);
-    }
-
-    [ContextMenu("Trigger Feedbacks")]
-    private void StartTheFeedbacks()
-    {
         StartCoroutine(TriggerFeedbacks());
     }
-
     private IEnumerator TriggerFeedbacks()
     {
-        ScaleApparition();
-        yield return new WaitForSeconds(timeBeforeChangingColor);
-        ChangeColor();
-    }
-
-    private void ScaleApparition()
-    {
         transform.DOScale(baseScale, changeScaleDuration);
+        renderer.material.DOColor(endColor, changeColorDuration);
+        yield return new WaitForSeconds(timeBeforeDestroyWhenFinished);
+        Destroy(this.gameObject);
     }
 }
