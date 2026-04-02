64844827292e43fbd69b4847aed73ad0fce1503a
Rob11005 <langerobin05@gmail.com>
2026-04-02 12:41:47 +0200

    done

--- Assets/_Player/Player_Script/StateMachine/States/DashState.cs
+++ Assets/_Player/Player_Script/StateMachine/States/DashState.cs
@@ -15,10 +15,12 @@ public class DashState : PlayerState
         //Debug.Log("J'entre dans l'état : " + stateMachine.currentPlayerState);
         //player.dashSound.Play();
         player.dash.StartDash();
+        player.animator.SetBool("isDashing", true);
     }
     public override void ExitState()
     {
         player.hitbox.enabled = true;
+        player.animator.SetBool("isDashing", false);
     }
     
     public override void FrameUpdate()
