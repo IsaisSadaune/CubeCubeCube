916dc82474bc3d391ddd6efb93dd8f0700adfd6d
Preonzz <tomempis05@gmail.com>
2026-04-15 11:05:48 +0200

    Starting transi title card

--- Assets/Scripts/Scripts_UI/MainMenu_Scripts/TextScroll_script.cs
+++ Assets/Scripts/Scripts_UI/MainMenu_Scripts/TextScroll_script.cs
@@ -10,15 +10,18 @@ public class TextScroll_script : MonoBehaviour
 
     private void Update()
     {
-        if (transform.position.x <= 0 && !hasInstantiated)
+        if (parentTransformScript.scrollingEnabled)
         {
-            hasInstantiated = true;
-            Instantiate(titleToInstantiate, instantiatePosition.position, Quaternion.identity, parentTransformScript.parentTransform);
+            if (transform.position.x <= 0 && !hasInstantiated)
+            {
+                hasInstantiated = true;
+                Instantiate(titleToInstantiate, instantiatePosition.position, Quaternion.identity, parentTransformScript.parentTransform);
+            }
+
+            if (transform.position.x <= destroyPosition.position.x)
+                Destroy(this.gameObject);
+
+            transform.position = new Vector3(transform.position.x - scrollSpeed * Time.deltaTime, transform.position.y, transform.position.z);
         }
-
-        if (transform.position.x <= destroyPosition.position.x)
-            Destroy(this.gameObject);
-
-        transform.position = new Vector3(transform.position.x - scrollSpeed * Time.deltaTime, transform.position.y, transform.position.z);
     }
 }
