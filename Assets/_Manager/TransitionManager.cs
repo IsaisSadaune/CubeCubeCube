69ede2490ce0f4173ae1f8d3b40c43e56b9a197c
Isais <52676440+IsaisSadaune@users.noreply.github.com>
2026-04-16 14:36:34 +0200

    ajout swap scene

---
+++ Assets/_Manager/TransitionManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class TransitionManager
+{
+
+
+    public static void GoToHub() => SceneManager.LoadScene("HubV1", LoadSceneMode.Single);
+    public static void GoToBoss1() => SceneManager.LoadScene("Scene_DiceBoss_Actual", LoadSceneMode.Single);
+}
