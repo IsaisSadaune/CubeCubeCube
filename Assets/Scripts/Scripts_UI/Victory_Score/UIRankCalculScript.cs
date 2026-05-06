29cb99bb323e9995b75e5cef375cff8598deee6b
Preonzz <tomempis05@gmail.com>
2026-05-06 11:36:03 +0200

    Changed score calculation

--- Assets/Scripts/Scripts_UI/Victory_Score/UIRankCalculScript.cs
+++ Assets/Scripts/Scripts_UI/Victory_Score/UIRankCalculScript.cs
@@ -6,78 +6,39 @@ public class UIRankCalculScript : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private TextMeshProUGUI bossNameText;
-    [SerializeField] private TextMeshProUGUI timeText, parryText, healText, lostRageText, rankText, pBText;
+    [SerializeField] private TextMeshProUGUI timeText, rankText, pBText;
     public BossScoreRequirementsScriptable scoreRequirements;
     public Button buttonToSelect;
 
-    private int finalScore;
     public char finalRank { get; private set; }
-
-    private void Start()
-    {
-        finalScore = 0;
-    }
 
     public void CalculateAndDisplayVictoryDatas(float secondsToBeat, int nbrParryDone, int nbrHealsUsed, float lostRage, int bossnumber)
     {
-        finalScore = 0;
-        TimeScore(secondsToBeat);
-        ParryScore(nbrParryDone);
-        PointLoss(nbrHealsUsed, lostRage);
+        DetermineRank(secondsToBeat);
 
-        DetermineRank();
-
-        UpdateText(secondsToBeat, nbrParryDone, nbrHealsUsed, lostRage);
+        UpdateText(secondsToBeat);
         UpdatePB(bossnumber);
     }
 
 
     #region CalculateScore
-    void TimeScore(float timeToBeatBoss)
+
+    void DetermineRank(float secondsToBeat)
     {
-        float timeScore = scoreRequirements.startTimeScore - (scoreRequirements.scoreLossPerSecond * timeToBeatBoss);
-
-        if (timeScore <= 0)
-            timeScore = 0;
-
-        finalScore += Mathf.RoundToInt(timeScore);
-        //Debug.Log(timeScore);
-    }
-
-    void ParryScore(int nbrParryDone)
-    {
-        if (nbrParryDone > scoreRequirements.maxParryAmount)
-            nbrParryDone = scoreRequirements.maxParryAmount;
-
-        finalScore += nbrParryDone * scoreRequirements.pointsPerParry;
-        //Debug.Log(finalScore);
-    }
-
-    void PointLoss(int nbrHealsUsed, float rageLost)
-    {
-        finalScore -= nbrHealsUsed * scoreRequirements.pointsLostPerHeal;
-
-        float timesRageLost = rageLost / scoreRequirements.lostRageToTriggerPointLoss;
-        finalScore -= (int)timesRageLost * scoreRequirements.pointsLostPerRageLoss;
-        //Debug.Log(finalScore);
-    }
-
-    void DetermineRank()
-    {
-        if (finalScore >= scoreRequirements.scoreForS)
+        if (secondsToBeat <= scoreRequirements.timeForS)
             finalRank = 'S';
-        else if (finalScore < scoreRequirements.scoreForS && finalScore >= scoreRequirements.scoreForA)
+        else if (secondsToBeat > scoreRequirements.timeForS && secondsToBeat <= scoreRequirements.timeForA)
             finalRank = 'A';
-        else if (finalScore < scoreRequirements.scoreForA && finalScore >= scoreRequirements.scoreForB)
+        else if (secondsToBeat > scoreRequirements.timeForA && secondsToBeat <= scoreRequirements.timeForB)
             finalRank = 'B';
-        else if (finalScore < scoreRequirements.scoreForB && finalScore >= scoreRequirements.scoreForC)
+        else if (secondsToBeat > scoreRequirements.timeForB && secondsToBeat <= scoreRequirements.timeForC)
             finalRank = 'C';
-        else if (finalScore < scoreRequirements.scoreForC)
+        else if (secondsToBeat > scoreRequirements.timeForC)
             finalRank = 'D';
     }
     #endregion
 
-    void UpdateText(float secondsToBeat, int nbrParryDone, int nbrHealUsed, float lostRage)
+    void UpdateText(float secondsToBeat)
     {
         bossNameText.SetText(scoreRequirements.bossName);
 
@@ -85,10 +46,6 @@ public class UIRankCalculScript : MonoBehaviour
         float unroundedSeconds = secondsToBeat % 60;
         float seconds = Mathf.Round(unroundedSeconds * 100) / 100;
         timeText.SetText(minutes + ":" + seconds);
-
-        parryText.SetText(nbrParryDone + "/" + scoreRequirements.maxParryAmount);
-        healText.SetText(nbrHealUsed.ToString());
-        lostRageText.SetText(lostRage.ToString());
 
         rankText.SetText(finalRank.ToString());
 
