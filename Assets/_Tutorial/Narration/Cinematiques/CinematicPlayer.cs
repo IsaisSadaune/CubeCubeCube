7e6fabeeee2cdd7bf17ddf95982a69cbc5559313
Isais <isais.sadaune@free.fr>
2026-05-08 14:34:59 +0100

    partie 1 tuto partiellement finie

---
+++ Assets/_Tutorial/Narration/Cinematiques/CinematicPlayer.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class CinematicPlayer : MonoBehaviour
+{
+    public abstract void PlayCinematic();
+}
---
+++ Assets/_Tutorial/Narration/Cinematiques/Tuto_Cinematic1.cs
@@ -0,0 +1,43 @@
+using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public class Tuto_Cinematic1 : CinematicPlayer
+{
+    [SerializeField] private GameObject CubeInf1;
+    [SerializeField] private Player player;
+    [SerializeField] private GameObject dialogue;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1f);
+        PlayCinematic();
+    }
+
+    public override void PlayCinematic()
+    {
+        //Sequence s = DOTween.Sequence();
+        ////Le joueur ne peut pas bouger
+        //s.Append(CubeInf1.transform.DOMoveY(15f, 1).SetEase(Ease.OutBack));
+        //s.AppendCallback(() => dialogue.SetActive(true));
+        //s.AppendInterval(1f);
+        //s.AppendCallback(() => dialogue.SetActive(false));
+        //s.AppendInterval(0.5f);
+        ////Cube Disparait (leger glitch) et se désactive
+        //s.Append(CubeInf1.transform.DOMoveZ(-25f, 1).SetEase(Ease.InBack))
+        //    .OnComplete(() => CubeInf1.SetActive(false));
+
+        //Test 2
+        Sequence s = DOTween.Sequence();
+        //Le joueur ne peut pas bouger
+        s.Append(CubeInf1.transform.DOMoveY(15f, 1).SetEase(Ease.OutBack));
+        s.Join(dialogue.transform.DOMoveY(15f,1).SetEase(Ease.OutBack));
+        s.AppendInterval(1.5f);
+        //Cube Disparait (leger glitch) et se désactive
+        s.Append(CubeInf1.transform.DOMoveZ(-25f, 1).SetEase(Ease.InBack))
+            .OnComplete(() => CubeInf1.SetActive(false));
+
+        //Le joueur peut bouger
+        throw new System.NotImplementedException();
+    }
+}
