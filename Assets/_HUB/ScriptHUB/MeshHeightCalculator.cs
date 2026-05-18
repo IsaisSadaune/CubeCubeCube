9e80838bb488ff6020e01af845f2006859edf417
JoachimLaine <joachim.laine37@gmail.com>
2026-05-18 13:57:08 +0200

    seconde tentative

---
+++ Assets/_HUB/ScriptHUB/MeshHeightCalculator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MeshHeightCalculator : MonoBehaviour
+{
+    public Mesh mesh;
+    public float height;
+    public Material mat;
+    public int matIndex;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mesh = gameObject.GetComponent<MeshFilter>().mesh;
+        mat = gameObject.GetComponent<MeshRenderer>().materials[matIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        height = mesh.bounds.size.y;
+        mat.SetFloat("height", height);
+    }
+}
