417297570b9b14ac8cc2e192ed8a8b43e87980d8
Isais <52676440+IsaisSadaune@users.noreply.github.com>
2026-05-27 14:49:00 +0200

    Modif condition player dead

--- Assets/_Boss/Boss_Proto/Scripts_Boss/Behaviors/PlayerDeathCondition.cs
+++ Assets/_Boss/Boss_Proto/Scripts_Boss/Behaviors/PlayerDeathCondition.cs
@@ -10,8 +10,35 @@ public partial class PlayerDeathCondition : Condition
 
     public override bool IsTrue()
     {
-        Debug.Log(Player.Value);
-        return Player.Value == null || Player.Value.GetComponent<Player>().hps.current_hp <= 0;
+        if (Player == null || Player.Value == null)
+        {
+            Debug.Log("Player n'est pas détecté");
+            return true;
+        }
+
+        Player playerComponent = Player.Value.GetComponent<Player>();
+
+        if (playerComponent == null)
+        {
+            Debug.LogWarning("Composant Player introuvable sur le GameObject");
+            return true;
+        }
+
+        Debug.Log(playerComponent);
+
+        if (playerComponent.hps == null)
+        {
+            Debug.LogWarning("hps non initialisé");
+            return false;
+        }
+
+        if (playerComponent.hps.current_hp <= 0)
+        {
+            Debug.Log("Player a 0 HPs");
+            return true;
+        }
+
+        return false;
     }
 
     public override void OnStart()
