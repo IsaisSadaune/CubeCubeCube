5cc7a0fd3be791e20ba3483da5d86be4bf676014
Rob11005 <langerobin05@gmail.com>
2026-06-02 12:27:44 +0200

    boss tuto

---
+++ Assets/_Tutorial/Boss Tuto/BossTuto.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class BossTuto : MonoBehaviour
+{
+    [SerializeField] private GameObject cubePrefab;
+    [SerializeField] private float cubeSpeed = 10f;
+    bool pattern1 = false;
+    bool pattern2 = true;
+    Boss_Variables variables;
+    void Start()
+    {
+        variables = GetComponent<Boss_Variables>();
+    }
+    public void StartBattle()
+    {
+        StartCoroutine(Patterns());
+    }
+
+    void SendCubesAway()
+    {
+        //Envoyez des cubes tout partout
+        int directions = 15;
+
+        for (int i = 0; i < directions; i++)
+        {
+            float angle = i * (360f / directions);
+
+            Vector3 dir = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+
+            GameObject cube = Instantiate(cubePrefab, transform.position, Quaternion.identity);
+
+            cube.transform.DOMove(transform.position + dir * 50f, 3f).SetEase(Ease.Linear).OnComplete(()=> Destroy(cube));
+        }
+    }
+    IEnumerator Patterns()
+    {
+        yield return new WaitForSeconds(1.5f);
+        while (variables.HP > 0)
+        {
+            if (!pattern1)
+            {
+                pattern1 = true;
+                for (int i = 0; i < 2; i++)
+                {
+                    yield return transform.DOMoveY(20f, 0.5f)
+                        .SetEase(Ease.Linear)
+                        .WaitForCompletion();
+
+                    yield return transform.DOMoveY(13f, 0.2f)
+                        .SetEase(Ease.Linear)
+                        .WaitForCompletion();
+                }
+                pattern2 = false;
+            }
+            else if (!pattern2)
+            {
+                pattern2 = true;
+
+                yield return transform.DOScale(0.75f, 0.5f)
+                    .WaitForCompletion();
+
+                yield return transform.DOScale(2f, 0.2f)
+                    .WaitForCompletion();
+
+                SendCubesAway();
+                pattern1 = false;
+            }
+
+            yield return transform.DORotate(new Vector3(0f, 180f, 0f),0.5f,RotateMode.LocalAxisAdd).WaitForCompletion();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
