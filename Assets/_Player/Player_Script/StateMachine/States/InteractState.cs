10c57a2603eac4db26acfa20f1c78f96e551f2ab
JoachimLaine <joachim.laine37@gmail.com>
2026-06-04 15:07:37 +0200

    PNJ talk marche à 2/3

--- Assets/_Player/Player_Script/StateMachine/States/InteractState.cs
+++ Assets/_Player/Player_Script/StateMachine/States/InteractState.cs
@@ -10,6 +10,7 @@ public class InteractState : PlayerState
     public override void EnterState()
     {
         base.EnterState();
+        AudioManager.Instance.PlaySound("Launch PNJ Talk");
         player.rb.linearVelocity = new Vector3(Vector3.zero.x, player.rb.linearVelocity.y, Vector3.zero.z);
         player.playerInput.SwitchCurrentActionMap("UI");
         player.dialogueCanvas.SetActive(true);
@@ -20,6 +21,7 @@ public class InteractState : PlayerState
 
     public override void ExitState()
     {
+        AudioManager.Instance.PlaySound("Finish PNJ Talk");
         player.pnj.StopAllCoroutines();
         player.pnj.emptyDialogueText.text = "";
         player.pnj.emptyNameText.text = "";
